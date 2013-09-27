7638a57c3c716ce582c95c7f71eccc79929e5e8b
admdrew <admdrew@gmail.com>
2013-09-27 15:26:21 -0500

    initial commit

---
+++ tc-builder.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name        TC Builder
+// @description JS-based test case creation/conversion tool
+// @author		Andy George
+// @version     2013-09-27
+// ==/UserScript==
+
+alert("this is a test");
